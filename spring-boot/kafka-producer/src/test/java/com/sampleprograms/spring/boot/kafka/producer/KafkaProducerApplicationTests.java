07b67470b50b25cec4b0b8ef13a40620c5c6c07a
Prashant <Prashant@DESKTOP-H7HHKQK.attlocal.net>
2019-01-23 22:17:10 -0600

    Addition of Kafka Producer

---
+++ spring-boot/kafka-producer/src/test/java/com/sampleprograms/spring/boot/kafka/producer/KafkaProducerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sampleprograms.spring.boot.kafka.producer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaProducerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
