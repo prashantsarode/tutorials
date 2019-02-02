6bed0e8b3aa40eff3b3e96979485cb725579de8f
Prashant <Prashant@DESKTOP-H7HHKQK>
2019-02-02 16:34:43 -0600

    Adding Internationalization to rest service

---
+++ spring-boot/restful-web-services/src/main/java/com/sampleprograms/spring/boot/rest/webservices/config/LocalConfig.java
@@ -0,0 +1,28 @@
+package com.sampleprograms.spring.boot.rest.webservices.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Component
+public class LocalConfig {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("langs/messages");
+		return messageSource;
+	}
+	
+}
--- spring-boot/restful-web-services/src/main/java/com/sampleprograms/spring/boot/rest/webservices/helloWorld/HelloWorld.java
+++ spring-boot/restful-web-services/src/main/java/com/sampleprograms/spring/boot/rest/webservices/helloWorld/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.sampleprograms.spring.boot.rest.webservices.helloWorld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	// GET
 	// URI - /hello-world
 	// Method "Hello World"
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
-	public String helloWorld() {
-		return "Hello World!!!";
+	public String helloWorld(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("hello.world.message", null, locale);
 	}
 
 	@GetMapping(path = "/hello-world-bean")
